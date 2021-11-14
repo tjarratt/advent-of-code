f70e6ec7087830762da86b6d6b7c66c621074ea9
Tim Jarratt <tjarratt@gmail.com>
2021-11-14 18:02:57 +0100

    Refactor ruby day 3

--- 2015/ruby/day-03/2.rb
+++ 2015/ruby/day-03/2.rb
@@ -1,44 +1,41 @@
 #!/usr/bin/env ruby
 
-x, y = 0, 0
-rx, ry = 0, 0
-grid = Hash.new(0)
-grid[[x, y]] = 1
 
-directions = File.read('input.txt')
-                 .split('')
-                 .each_with_index
-                 .partition { |_, i| i % 2 == 0 }
-
-directions.first.map do |direction, _|
-  case direction
-    when '^'
-        y = y+1
-    when '<'
-        x = x-1
-    when '>'
-        x = x+1
-    when 'v'
-        y = y-1
-  end
-
-  grid[[x, y]] += 1
+def main
+  directions = File.read('input.txt')
+                   .split('')
+                   .each_with_index
+                   .partition { |_, i| i % 2 == 0 }
+  
+  grid = Hash.new(0)
+  grid[[0, 0]] = 1
+  
+  count_visits(grid, directions.first)
+  count_visits(grid, directions.last)
+  
+  puts grid.keys.size
 end
 
-directions.last.map do |direction, _|
-  case direction
-    when '^'
-        ry = ry+1
-    when '<'
-        rx = rx-1
-    when '>'
-        rx = rx+1
-    when 'v'
-        ry = ry-1
-  end
+def count_visits(grid, directions)
+  x, y = 0, 0
 
-  grid[[rx, ry]] += 1
+  directions.map do |direction, _|
+    case direction
+      when '^'
+          y = y+1
+      when '<'
+          x = x-1
+      when '>'
+          x = x+1
+      when 'v'
+          y = y-1
+    end
+  
+    grid[[x, y]] += 1
+  end
+  
+  grid.keys.size
 end
 
-puts grid.keys.size
+main
 
