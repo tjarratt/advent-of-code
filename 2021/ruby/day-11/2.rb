5273bd8407a7c68d355dffbd3c5be9020534dc2b
Tim Jarratt <tjarratt@gmail.com>
2021-12-11 10:43:01 +0100

    2021 day 11; part 2; ruby
    A nice lay-up. A good day for refactoring.

---
+++ 2021/ruby/day-11/2.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+
+def main
+    grid = File.read('input.txt')
+               .split("\n")
+               .map { _1.split('').map(&:to_i) }
+
+    1000.times do |round|
+        grid.each_with_index do |row, y|
+            row.each_with_index do |oct, x|
+                grid[y][x] += 1
+            end
+        end
+
+        flashed = []
+        octopi = flashing(grid)
+        while octopi.any?
+            octopi.each do |x, y|
+                flashed << [x, y]
+
+                neighbors(grid, x, y).each do |nx, ny|
+                    next if flashed.include? [nx, ny]
+                    grid[ny][nx] += 1
+                end
+            end 
+
+            octopi = flashing(grid) - flashed
+        end
+
+        flashed.each do |x, y|
+            grid[y][x] = 0
+        end
+
+        if flashed.size == size(grid)
+            puts "all flashed at #{round + 1}"
+            exit
+        end
+    end
+end
+
+def size(grid)
+    grid.size * grid.first.size
+end
+
+def pp(grid)
+    grid.each do |line|
+        puts line.join(' ')
+    end
+
+    puts
+end
+
+def neighbors(grid, x, y)
+    neighbors = []
+    neighbors << [x - 1, y] unless x == 0
+    neighbors << [x + 1, y] unless x == grid.first.size - 1
+    neighbors << [x, y - 1] unless y == 0
+    neighbors << [x, y + 1] unless y == grid.size - 1
+    
+    neighbors << [x - 1, y - 1] unless x == 0 or y == 0
+    neighbors << [x - 1, y + 1] unless x == 0 or y == grid.size - 1
+    neighbors << [x + 1, y - 1] unless x == grid.first.size - 1 or y == 0
+    neighbors << [x + 1, y + 1] unless x == grid.first.size - 1 or y == grid.size - 1
+
+    neighbors
+end
+
+def flashing(grid)
+    results = []
+    grid.each_with_index do |row, y|
+        row.each_with_index do |octo, x|
+            results << [x, y] if octo > 9
+        end
+    end
+
+    results
+end
+
+main
