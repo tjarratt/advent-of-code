84d85ce3df6665c9043f13110a8da97d3d8512cd
Tim Jarratt <tjarratt@gmail.com>
2022-12-01 15:21:30 +0100

    refactor 2022 part 2 in ruby

--- ruby/day-01/part-2.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-calories = File.read("input.txt")
-                .split("\n\n")
-                .map {|l| l.split("\n").map(&:to_i).inject(:+) }
-
-puts calories.sort[-3..-1].inject(:+)
--- ruby/day-01/solution.rb
+++ ruby/day-01/solution.rb
@@ -4,4 +4,8 @@ calories = File.read("input.txt")
                 .split("\n\n")
                 .map {|l| l.split("\n").map(&:to_i).inject(:+) }
 
-puts calories.sort.last
+
+puts "part 1 solution is : #{calories.sort.last}"
+puts "part 2 solution is : #{calories.sort[-3..-1].inject(:+)}"
+
+
