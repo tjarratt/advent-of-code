a182ae16562fd9e7a15ff4a2858f26d0f7944914
Tim Jarratt <tjarratt@gmail.com>
2022-12-04 19:41:12 +0100

    2022; ruby ; day 04 ; part 2

--- 2022/ruby/day-04/solution.rb
+++ 2022/ruby/day-04/solution.rb
@@ -12,4 +12,15 @@ def part_one
     puts result
 end
 
+def part_two
+    result = File.read('input')
+                 .split("\n")
+                 .map {|line| line.split(',')
+                                  .map {|range| from, to = range.split('-'); from.upto(to).to_a.to_set } }
+                 .inject(0) {|sum, pair| sum += 1 unless (pair.first & pair.last).empty? ; sum }
+    puts result
+end
+
 part_one
+part_two
+
