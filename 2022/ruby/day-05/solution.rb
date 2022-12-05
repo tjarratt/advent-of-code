6fc815a72aa02c5922a11f88554d27300c8446fa
Tim Jarratt <tjarratt@gmail.com>
2022-12-05 22:17:25 +0100

    2022; ruby ; day 05 ; part 2

--- 2022/ruby/day-05/solution.rb
+++ 2022/ruby/day-05/solution.rb
@@ -1,26 +1,56 @@
 #!/usr/bin/env ruby
 
-head, tail = File.read('input').split("\n\n")
-cargo = head.split("\n")
-            .map {|line| line.scan /(\s{4}|\[[A-Z]\])/ }
-            .reject(&:empty?)
-            .map {|captures| captures.map {|place| place.first.match(/\s+/) ? [] : [place.first.split('')[1]] } }
-            .transpose
-            .map(&:reverse)
-            .map {|stack| stack.reject(&:empty?) }
+def part_one
+    head, tail = File.read('input').split("\n\n")
+    cargo = head.split("\n")
+                .map {|line| line.scan /(\s{4}|\[[A-Z]\])/ }
+                .reject(&:empty?)
+                .map {|captures| captures.map {|place| place.first.match(/\s+/) ? [] : [place.first.split('')[1]] } }
+                .transpose
+                .map(&:reverse)
+                .map {|stack| stack.reject(&:empty?) }
+    
+    instructions = tail.split("\n")
+                       .map {|line| line.scan /move (\d+) from (\d+) to (\d+)/ }
+                       .flatten(1)
+                       .map {|arr| arr.map(&:to_i) }
+    
+    instructions.each do |quantity, from, to|
+        from, to = [from, to].map(&:pred)
 
-instructions = tail.split("\n")
-                   .map {|line| line.scan /move (\d+) from (\d)+ to (\d+)/ }
-                   .flatten(1)
-                   .map {|arr| arr.map(&:to_i) }
-
-instructions.each do |quantity, from, to|
-    from, to = [from, to].map(&:pred)
-
-    quantity.times do
-        cargo[to] << cargo[from].pop
+        quantity.times do
+            cargo[to] << cargo[from].pop
+        end
     end
+    
+    puts cargo.map(&:last).map(&:first).join('')
 end
 
-puts cargo.map(&:last).map(&:first).join('')
+def part_two
+    head, tail = File.read('input').split("\n\n")
+    cargo = head.split("\n")
+                .map {|line| line.scan /(\s{4}|\[[A-Z]\])/ }
+                .reject(&:empty?)
+                .map {|captures| captures.map {|place| place.first.match(/\s+/) ? [] : [place.first.split('')[1]] } }
+                .transpose
+                .map(&:reverse)
+                .map {|stack| stack.reject(&:empty?) }
 
+    instructions = tail.split("\n")
+                       .map {|line| line.scan /move (\d+) from (\d+) to (\d+)/ }
+                       .flatten(1)
+                       .map {|arr| arr.map(&:to_i) }
+
+    instructions.each do |quantity, from, to|
+        from, to = [from, to].map(&:pred)
+
+        removed = cargo[from][-quantity..-1]
+        quantity.times { cargo[from].pop }
+        removed.each {|c| cargo[to].push(c) }
+    end
+
+    puts cargo.map(&:last).map(&:first).join('')
+end
+
+part_one
+part_two
