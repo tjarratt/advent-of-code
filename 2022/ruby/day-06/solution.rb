37358b2c6b0c9511198eecfc36bc67ec7ef06d6e
Tim Jarratt <tjarratt@gmail.com>
2022-12-06 09:46:44 +0100

    2022; ruby ; day 06 ; part 2

--- 2022/ruby/day-06/solution.rb
+++ 2022/ruby/day-06/solution.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 
-puts File.read('input')
+def detect_after(how_many)
+    File.read('input')
          .split('')
-         .each_cons(4)
+         .each_cons(how_many)
          .each_with_index
          .filter {|chars, index| chars.uniq.size == chars.size }
          .first
-         .last + 4
+         .last + how_many
+end
+
+puts detect_after(4)
+puts detect_after(14)
