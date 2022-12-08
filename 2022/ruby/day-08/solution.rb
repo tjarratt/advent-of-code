21a4328742c61e5f03be520c4ba9d458418500c3
Tim Jarratt <tjarratt@gmail.com>
2022-12-08 23:40:07 +0100

    2022; ruby ; day 08 ; part 2
    No time for anything really clever

--- 2022/ruby/day-08/solution.rb
+++ 2022/ruby/day-08/solution.rb
@@ -7,7 +7,8 @@ class Tree
  
     def initialize(height)
         @height = height
-        @occlusions = Set.new    
+        @occlusions = Set.new
+        @visibilities = Hash.new
     end
 
     def occluded(dir)
@@ -16,6 +17,14 @@ class Tree
 
     def visible?
         @occlusions.size < 4
+    end
+
+    def can_see(direction, how_many)
+        @visibilities[direction] = how_many 
+    end
+
+    def scenic_score
+        @visibilities.values.inject(:*)
     end
 end
 
@@ -68,4 +77,50 @@ def part_one
     puts forest.filter(&:visible?).size
 end
 
+def part_two
+    grid = File.read('input')
+               .split("\n")
+               .map { _1.split('').map(&:to_i).map { |value| Tree.new(value) } }
+    
+    forest = Set.new
+    0.upto(grid.size - 1) do |y|
+        0.upto(grid.first.size - 1) do |x|
+            tree = grid[y][x]
+
+            how_many = 0
+            (x-1).downto(0) do |dx|
+                how_many += 1
+                break if grid[y][dx].height >= tree.height
+            end
+            tree.can_see(:left, how_many)
+
+            how_many = 0
+            (x+1).upto(grid.first.size - 1) do |dx|
+                how_many += 1
+                break if grid[y][dx].height >= tree.height
+            end
+            tree.can_see(:right, how_many)
+
+            how_many = 0
+            (y-1).downto(0) do |dy|
+                how_many += 1
+                break if grid[dy][x].height >= tree.height
+            end
+            tree.can_see(:top, how_many)
+
+            how_many = 0
+            (y+1).upto(grid.size - 1) do |dy|
+                how_many += 1
+                break if grid[dy][x].height >= tree.height
+            end
+            tree.can_see(:bottom, how_many)
+
+            forest << tree
+        end
+    end
+
+    puts forest.map(&:scenic_score).sort.last
+end
+
 part_one
+part_two
