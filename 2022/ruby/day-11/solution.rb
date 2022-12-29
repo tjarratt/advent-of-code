b05e036c2843f91de9be307409c04dc1447d64b1
Tim Jarratt <tjarratt@gmail.com>
2022-12-29 21:43:39 +0100

    2022; ruby ; day 11 ; part 2
    Holy crap.
    
    Ran into a few problems here. Performance for one thing,
    the loop turned into crawl around 700 iterations. Looking
    at the performance of the code, it appears that the problem was
    that we had very large numbers (BigNum) and that calling #to_s
    on them would take increasingly long. This required parsing the
    expressions properly, not using eval, and making the memory usage
    and runtime complexity for this much simpler.
    
    After that, we had warnings with some multiplication operations being
    too large for ruby to handle natively. The problem refers to the need to
    'find a creative way to keep your worry levels managable'. The logical way
    to do this is to take the worry levels modulo the least common multiple
    of all the divisors, since that does not change the modulo operation.
    
    Phew. Good stuff.

--- 2022/ruby/day-11/solution.rb
+++ 2022/ruby/day-11/solution.rb
@@ -23,8 +23,44 @@ def part_one
     # multiply their most seen item
 end
 
+def part_two
+    monkeys = File.read('input')
+                  .split("\n\n")
+                  .map { parse_monkey(_1) }
+
+    lcm = monkeys.map(&:test_if_divisible_by).inject(:*)
+    monkeys.each { _1.divisor = lcm }
+
+    10000.times do |i|
+        monkeys.each do |monkey|
+            monkey.inspect_decide_throw do |item, index|
+                raise 'FREAKOUT' if index >= monkeys.size
+                monkeys[index].receive(item)
+            end
+        end
+    end
+
+    puts monkeys.map(&:inspections).sort.last(2).inject(:*)
+end
+
+class MonkeyWithATimer
+    def initialize(monkey)
+        @delegate = monkey
+    end
+
+    def method_missing(m, *args, &block)
+        start = Time.now
+        result = @delegate.send(m, *args, &block)
+        finish = Time.now
+        puts "Resolved #{m} in #{finish - start} seconds"
+
+        result
+    end
+end
+
 class Monkey
-    attr_reader :inspections
+    attr_reader :inspections, :test_if_divisible_by
+    attr_accessor :divisor
 
     def initialize(items, operation, test, if_true, if_false)
         @inspections = 0
@@ -48,14 +84,24 @@ class Monkey
         @items = @items.map { apply_operation_to(item: _1) }
                        .map { _1 / 3 }
                        .map { [_1, decide_monkey_for(item: _1)] }
-                       .each { @inspections += 1 }
+                       .tap { @inspections += _1.size }
                        .each { yield(*_1) }
                        .then { [] }
     end
 
+    def inspect_decide_throw
+        @items = @items.map { apply_operation_to(item: _1) }
+                       .map { _1 % @divisor }
+                       .map { [_1, decide_monkey_for(item: _1)] }
+                       .tap { @inspections += _1.size }
+                       .each { yield(*_1) }
+                       .then { [] }
+
+    end
+
     private
     def apply_operation_to(item: Number)
-        eval(@operation.gsub('old', item.to_s))
+        @operation.call(item)
     end
 
     def decide_monkey_for(item: Number)
@@ -67,11 +113,24 @@ def parse_monkey(block)
     lines = block.split("\n")
     Monkey.new(
         lines[1].split(': ').last.split(', ').map(&:to_i),
-        lines[2].split('= ').last,
+        parse_operation(expression: lines[2].split('= ').last),
         lines[3].split(' ').last.to_i,
         lines[4].split(' ').last.to_i,
         lines[5].split(' ').last.to_i
     )
 end
 
+def parse_operation(expression: String)
+    tokens = expression.split(' ')
+    case tokens
+    in ['old', '*', 'old']
+        return Proc.new { _1 ** 2 }
+    in ['old', '+', String]
+        return Proc.new { _1 + tokens.last.to_i }
+    in ['old', '*', String]
+        return Proc.new { _1 * tokens.last.to_i }
+    end
+end
+
 part_one
+part_two
