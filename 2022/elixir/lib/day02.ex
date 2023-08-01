ca52141a6d915b0620f227e87c3fb353d2623ad7
Tim Jarratt <tjarratt@gmail.com>
2023-08-01 23:48:17 +0200

    2022; elixir; day 02; part 1

---
+++ 2022/elixir/lib/day02.ex
@@ -0,0 +1,84 @@
+defmodule Day02 do
+  def part_one do
+    "input"
+    |> read_file()
+    |> String.split("\n")
+    |> Enum.reject(fn str -> String.length(str) == 0 end)
+    |> Enum.map(fn line -> String.split(line, " ") end)
+    |> Enum.map(&parse/1)
+    |> Enum.map(&outcome/1)
+    |> Enum.map(fn [outcome, choice] -> score(outcome) + score(choice) end)
+    |> Enum.sum
+  end
+
+  def score(result) do
+    case result do
+      :win -> 6
+      :draw -> 3
+      :loss -> 0
+      :rock -> 1
+      :paper -> 2
+      :scissors -> 3
+    end
+  end
+
+  def outcome([opponent_choice, player_choice]) do
+    outcome = case [opponent_choice, player_choice] do
+      # draws
+      [:rock, :rock] -> :draw
+      [:paper, :paper] -> :draw
+      [:scissors, :scissors] -> :draw
+
+        # losses
+      [:rock, :scissors] -> :loss
+      [:paper, :rock] -> :loss
+      [:scissors, :paper] -> :loss
+
+        # wins
+      [:rock, :paper] -> :win
+      [:paper, :scissors] -> :win
+      [:scissors, :rock] -> :win
+    end
+
+    [outcome, player_choice]
+  end
+
+  def parse([opponent, player]) do
+    [parse(opponent), parse(player)]
+  end
+
+  def parse("A") do
+    :rock
+  end
+
+  def parse("B") do
+    :paper
+  end
+
+  def parse("C") do
+    :scissors
+  end
+
+  def parse("X") do
+    :rock
+  end
+
+  def parse("Y") do 
+    :paper
+  end
+
+  def parse("Z") do 
+    :scissors
+  end
+
+  def read_file(path) do
+    case File.read(Path.join(["resources", "2", path])) do
+      {:ok, content} ->
+        content
+
+      {:error, reason} ->
+        IO.puts("Error reading file #{path} : #{reason}")
+        nil
+    end
+  end
+end
--- 2022/elixir/lib/mix/tasks/aoc.ex
+++ 2022/elixir/lib/mix/tasks/aoc.ex
@@ -6,11 +6,16 @@ defmodule Mix.Tasks.Aoc do
     IO.puts(Day01.part_two())
   end
 
+  def run(["2"]) do
+    IO.puts(Day02.part_one())
+  end
+
+
   @shortdoc "Calculate solution(s) for Day 1"
   @moduledoc """
   A custom mix task that runs the code to calculate solutions for Advent of Code 2022
   """
   def run(_) do
-    IO.puts("That's the bees' knees")
+    IO.puts("Sorry, that day hasn't been written yet")
   end
 end
