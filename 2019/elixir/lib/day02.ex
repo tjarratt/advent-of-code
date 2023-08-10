587c5077fc2ec230363293ab13b8f163fd16eebb
Tim Jarratt <tjarratt@gmail.com>
2023-08-10 20:49:56 +0200

    2019; elixir; day 02; part 1

---
+++ 2019/elixir/lib/day02.ex
@@ -0,0 +1,54 @@
+defmodule Day02 do
+  def part_one do
+    "input"
+    |> read_input()
+    |> String.trim_trailing()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> List.replace_at(1, 12)
+    |> List.replace_at(2, 2)
+    |> (fn program -> {0, program} end).()
+    |> calculate()
+    |> elem(1)
+    |> Enum.at(0)
+  end
+
+  def part_two do
+    "<part two goes here>"
+  end
+
+  defp read_input(filename) do
+    path = Path.join(["resources", "02", filename])
+
+    case File.read(path) do
+      {:ok, contents} -> contents
+      {:error, _} -> raise "no such file #{path}"
+    end
+  end
+
+  defp calculate({ip, program}) do
+    opcode = Enum.at(program, ip)
+
+    case opcode do
+      1 -> {ip + 4, add(ip, program)} |> calculate()
+      2 -> {ip + 4, multiply(ip, program)} |> calculate()
+      99 -> {ip, program}
+    end
+  end
+
+  defp add(ip, program) do
+    a = Enum.at(program, ip + 1)
+    b = Enum.at(program, ip + 2)
+    addr = Enum.at(program, ip + 3)
+
+    List.replace_at(program, addr, Enum.at(program, a) + Enum.at(program, b))
+  end
+
+  def multiply(ip, program) do
+    a = Enum.at(program, ip + 1)
+    b = Enum.at(program, ip + 2)
+    addr = Enum.at(program, ip + 3)
+
+    List.replace_at(program, addr, Enum.at(program, a) * Enum.at(program, b))
+  end
+end
