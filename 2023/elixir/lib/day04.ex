dcbf0d1a1aecf1b68be0d5ac4a255a5796eec7a5
Tim Jarratt <tim.jarratt@maersk.com>
2023-12-04 17:48:13 +0100

    2023; elixir; day 04; part 2
    A bit tricky to follow the logic regarding how many copies to make but this worked out
    just fine. As an added benefit I learned to not use list comprehensions
    to attempt to update a list multiple times, it's much better and easier
    to do so using recursion.

--- 2023/elixir/lib/day04.ex
+++ 2023/elixir/lib/day04.ex
@@ -13,7 +13,36 @@ defmodule Day04 do
   end
 
   def part_two() do
-    "not yet"
+    "input"
+    |> read_file!()
+    |> String.split("\n")
+    |> Enum.reject(fn line -> String.length(line) == 0 end)
+    |> Enum.map(&parse_gamecard/1)
+    |> Enum.map(fn card -> {card, 1} end)
+    |> sum_up_scorecards(0)
+    |> Enum.map(fn {_card, num_copies} -> num_copies end)
+    |> Enum.sum()
+  end
+
+  defp sum_up_scorecards(cards, index) when index >= length(cards), do: cards
+
+  defp sum_up_scorecards(cards, index) do
+    {card, copies_to_make} = Enum.at(cards, index)
+    winning_numbers = count_winning_numbers(card)
+
+    updated_cards = copy_cards(cards, index, winning_numbers, copies_to_make)
+
+    sum_up_scorecards(updated_cards, index + 1)
+  end
+
+  defp copy_cards(cards, _index, offset, _copies_to_make) when offset == 0, do: cards
+
+  defp copy_cards(cards, index, offset, copies_to_make) do
+    cards
+    |> List.update_at(index + offset, fn {card, num_copies} ->
+      {card, num_copies + copies_to_make}
+    end)
+    |> copy_cards(index, offset - 1, copies_to_make)
   end
 
   defp points_for(0), do: 0
@@ -33,7 +62,8 @@ defmodule Day04 do
 
   defp parse_gamecard(line) do
     [game, rest] = String.split(line, ": ")
-    "Card " <> number = game
+    "Card " <> raw_number = game
+    number = raw_number |> String.trim_leading() |> String.to_integer()
 
     [winning_numbers, our_numbers] =
       rest
