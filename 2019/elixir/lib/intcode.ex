76ebfe42edc294e09c12927c629f29dae1678e18
Tim Jarratt <tim.jarratt@maersk.com>
2023-12-09 15:47:25 +0100

    Refactor intcode computer
    Adopt state-in, state-out as the interface for opcode handlers
    
    This makes the do_calculate() function much simpler, and makes
    it easier to do implement opcodes where the number of values the
    instruction pointer should be incremented by is dynamic

--- 2019/elixir/lib/intcode.ex
+++ 2019/elixir/lib/intcode.ex
@@ -3,7 +3,7 @@ defmodule Intcode do
     do_calculate({0, 0, program})
   end
 
-  defp do_calculate({ip, output_written, program}) do
+  defp do_calculate({ip, output_written, program} = state) do
     instruction = Enum.at(program, ip)
 
     {opcode, modes} = parse(opcode: instruction)
@@ -13,17 +13,108 @@ defmodule Intcode do
     # and then we always send there instead of send(self(), output)
     # this is less dynamic, but sets us up for success later on
     # when we need to connect intcode computers
+
     case opcode do
-      1 -> {ip + 4, output_written, add(ip, program, modes)} |> do_calculate()
-      2 -> {ip + 4, output_written, multiply(ip, program, modes)} |> do_calculate()
-      3 -> {ip + 2, output_written, read_input(ip, program)} |> do_calculate()
-      4 -> {ip + 2, output_written + 1, write_output(ip, program)} |> do_calculate()
-      7 -> {ip + 4, output_written, check_less_than(ip, program, modes)} |> do_calculate()
-      8 -> {ip + 4, output_written, check_equality(ip, program, modes)} |> do_calculate()
-      99 -> {ip, output_written, program}
-      _ -> raise "unexpected opcode #{opcode}"
+      1 ->
+        add(state, modes) |> do_calculate()
+
+      2 ->
+        multiply(state, modes) |> do_calculate()
+
+      3 ->
+        read_input(state) |> do_calculate()
+
+      4 ->
+        write_output(state) |> do_calculate()
+
+      7 ->
+        check_less_than(state, modes) |> do_calculate()
+
+      8 ->
+        check_equality(state, modes) |> do_calculate()
+
+      99 ->
+        {ip, output_written, program}
+
+      _ ->
+        raise "unexpected opcode #{opcode}"
     end
   end
+
+  # pragma mark - opcode handlers
+
+  defp read_input({ip, output_written, program}) do
+    value =
+      receive do
+        {:input, input} -> input
+      end
+
+    addr = Enum.at(program, ip + 1)
+
+    {ip + 2, output_written, List.replace_at(program, addr, value)}
+  end
+
+  defp write_output({ip, output_written, program}) do
+    addr = Enum.at(program, ip + 1)
+
+    send(self(), {:write_output, Enum.at(program, addr)})
+
+    {ip + 2, output_written + 1, program}
+  end
+
+  defp add({ip, output_written, program}, modes) do
+    a = read_parameter(1, ip, program, modes)
+    b = read_parameter(2, ip, program, modes)
+    sum = a + b
+
+    addr = Enum.at(program, ip + 3)
+
+    {ip + 4, output_written, List.replace_at(program, addr, sum)}
+  end
+
+  defp multiply({ip, output_written, program}, modes) do
+    a = read_parameter(1, ip, program, modes)
+    b = read_parameter(2, ip, program, modes)
+    product = a * b
+
+    addr = Enum.at(program, ip + 3)
+
+    {ip + 4, output_written, List.replace_at(program, addr, product)}
+  end
+
+  defp check_equality({ip, output_written, program}, modes) do
+    a = read_parameter(1, ip, program, modes)
+    b = read_parameter(2, ip, program, modes)
+
+    result =
+      if a == b do
+        1
+      else
+        0
+      end
+
+    addr = Enum.at(program, ip + 3)
+
+    {ip + 4, output_written, List.replace_at(program, addr, result)}
+  end
+
+  defp check_less_than({ip, output_written, program}, modes) do
+    a = read_parameter(1, ip, program, modes)
+    b = read_parameter(2, ip, program, modes)
+
+    result =
+      if a < b do
+        1
+      else
+        0
+      end
+
+    addr = Enum.at(program, ip + 3)
+
+    {ip + 4, output_written, List.replace_at(program, addr, result)}
+  end
+
+  # pragma mark - functions to read next instruction information
 
   defp parse(opcode: instruction) do
     digits = instruction |> Integer.to_string()
@@ -38,35 +129,6 @@ defmodule Intcode do
     }
   end
 
-  defp read_input(ip, program) do
-    value =
-      receive do
-        {:input, input} -> input
-      end
-
-    addr = Enum.at(program, ip + 1)
-
-    List.replace_at(program, addr, value)
-  end
-
-  defp write_output(ip, program) do
-    addr = Enum.at(program, ip + 1)
-
-    send(self(), {:write_output, Enum.at(program, addr)})
-
-    program
-  end
-
-  defp mode_for_index(modes, index) do
-    mode = Enum.at(modes, index - 1)
-
-    if mode == nil do
-      0
-    else
-      mode
-    end
-  end
-
   defp read_parameter(index, ip, program, modes) do
     case mode_for_index(modes, index) do
       0 ->
@@ -78,55 +140,13 @@ defmodule Intcode do
     end
   end
 
-  defp add(ip, program, modes) do
-    a = read_parameter(1, ip, program, modes)
-    b = read_parameter(2, ip, program, modes)
-    sum = a + b
+  defp mode_for_index(modes, index) do
+    mode = Enum.at(modes, index - 1)
 
-    addr = Enum.at(program, ip + 3)
-
-    List.replace_at(program, addr, sum)
-  end
-
-  defp multiply(ip, program, modes) do
-    a = read_parameter(1, ip, program, modes)
-    b = read_parameter(2, ip, program, modes)
-    product = a * b
-
-    addr = Enum.at(program, ip + 3)
-
-    List.replace_at(program, addr, product)
-  end
-
-  defp check_equality(ip, program, modes) do
-    a = read_parameter(1, ip, program, modes)
-    b = read_parameter(2, ip, program, modes)
-
-    result =
-      if a == b do
-        1
-      else
-        0
-      end
-
-    addr = Enum.at(program, ip + 3)
-
-    List.replace_at(program, addr, result)
-  end
-
-  defp check_less_than(ip, program, modes) do
-    a = read_parameter(1, ip, program, modes)
-    b = read_parameter(2, ip, program, modes)
-
-    result =
-      if a < b do
-        1
-      else
-        0
-      end
-
-    addr = Enum.at(program, ip + 3)
-
-    List.replace_at(program, addr, result)
+    if mode == nil do
+      0
+    else
+      mode
+    end
   end
 end
