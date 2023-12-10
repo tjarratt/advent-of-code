7fa5ce60aac44b311a589d070e8ffa19ac1363bc
Tim Jarratt <tim.jarratt@maersk.com>
2023-12-10 13:27:16 +0100

    2023; elixir; day 10; part 1
    A bit hairy, and a lot of cases to consider.
    There's got to be a better way to handle the connections from
    one pipe to another. Perhaps specifying for each where they can
    connect, and then checking to see if the connections do line up
    from neighbor to neighbor are possible ?

---
+++ 2023/elixir/lib/day10.ex
@@ -0,0 +1,159 @@
+defmodule Day10 do
+  use AocTemplate
+
+  def part_one() do
+    "input"
+    |> read_file!()
+    |> parse_matrix()
+    |> List.flatten()
+    |> (fn matrix ->
+          start = Enum.find(matrix, fn {_, _, char} -> char == "S" end)
+
+          matrix
+          |> find_neighbors(start)
+          |> Enum.filter(&connecting?(&1, start))
+          |> hd()
+          |> (fn node ->
+                {:closed_loop, loop} = forms_closed_loop?(matrix, node, [start], to: start)
+                loop
+              end).()
+        end).()
+    |> (fn loop -> (length(loop) / 2) |> trunc() end).()
+  end
+
+  defp forms_closed_loop?(matrix, current, previous_pipes, to: starting_point) do
+    if current == starting_point do
+      {:closed_loop, previous_pipes}
+    else
+      previous = List.last(previous_pipes)
+      neighbors = find_neighbors(matrix, current)
+      next_pipe = find_next_pipe(neighbors -- [previous], current)
+
+      case next_pipe do
+        nil ->
+          {:nope}
+
+        _ ->
+          forms_closed_loop?(matrix, next_pipe, previous_pipes ++ [current], to: starting_point)
+      end
+    end
+  end
+
+  defp connecting?({_y, x1, "|"}, {_w, x2, "|"}) when x1 == x2, do: true
+  defp connecting?({y1, x1, "|"}, {y2, x2, "L"}) when x1 == x2 and y2 == y1 + 1, do: true
+  defp connecting?({y1, x1, "|"}, {y2, x2, "J"}) when x1 == x2 and y2 == y1 + 1, do: true
+  defp connecting?({y1, x1, "|"}, {y2, x2, "7"}) when x1 == x2 and y2 == y1 - 1, do: true
+  defp connecting?({y1, x1, "|"}, {y2, x2, "F"}) when x1 == x2 and y2 == y1 - 1, do: true
+  defp connecting?({_y, x1, "|"}, {_w, x2, "S"}) when x1 == x2, do: true
+
+  defp connecting?({y1, _x, "-"}, {y2, _w, "-"}) when y1 == y2, do: true
+  defp connecting?({y1, x1, "-"}, {y2, x2, "L"}) when y1 == y2 and x2 == x1 - 1, do: true
+  defp connecting?({y1, x1, "-"}, {y2, x2, "F"}) when y1 == y2 and x2 == x1 - 1, do: true
+  defp connecting?({y1, x1, "-"}, {y2, x2, "7"}) when y1 == y2 and x2 == x1 + 1, do: true
+  defp connecting?({y1, x1, "-"}, {y2, x2, "J"}) when y1 == y2 and x2 == x1 + 1, do: true
+  defp connecting?({y1, _x, "-"}, {y2, _w, "S"}) when y1 == y2, do: true
+
+  defp connecting?({y1, x1, "L"}, {y2, x2, "|"}) when y2 == y1 - 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "L"}, {y2, x2, "F"}) when y2 == y1 - 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "L"}, {y2, x2, "-"}) when y2 == y1 and x2 == x1 + 1, do: true
+  defp connecting?({y1, x1, "L"}, {y2, x2, "J"}) when y2 == y1 and x2 == x1 + 1, do: true
+
+  defp connecting?({y1, x1, "L"}, {y2, x2, "7"})
+       when (y2 == y1 and x2 == x1 + 1) or (y2 == y1 - 1 and x2 == x1),
+       do: true
+
+  defp connecting?({y1, x1, "L"}, {y2, x2, "S"})
+       when (y2 == y1 and x2 == x1 + 1) or (y2 == y1 - 1 and x2 == x1),
+       do: true
+
+  defp connecting?({y1, x1, "J"}, {y2, x2, "|"}) when y2 == y1 - 1 and x1 == x2, do: true
+  defp connecting?({y1, x1, "J"}, {y2, x2, "7"}) when y2 == y1 - 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "J"}, {y2, x2, "-"}) when y2 == y1 and x2 == x1 - 1, do: true
+  defp connecting?({y1, x1, "J"}, {y2, x2, "L"}) when y2 == y1 and x2 == x1 - 1, do: true
+
+  defp connecting?({y1, x1, "J"}, {y2, x2, "F"})
+       when (y2 == y1 and x2 == x1 - 1) or (y2 == y1 - 1 and x2 == x1),
+       do: true
+
+  defp connecting?({y1, x1, "J"}, {y2, x2, "S"})
+       when (y2 == y1 and x2 == x1 - 1) or (y2 == y1 - 1 and x2 == x1),
+       do: true
+
+  defp connecting?({y1, x1, "7"}, {y2, x2, "|"}) when y2 == y1 + 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "7"}, {y2, x2, "J"}) when y2 == y1 + 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "7"}, {y2, x2, "-"}) when y2 == y1 and x2 == x1 - 1, do: true
+  defp connecting?({y1, x1, "7"}, {y2, x2, "F"}) when y2 == y1 and x2 == x1 - 1, do: true
+
+  defp connecting?({y1, x1, "7"}, {y2, x2, "L"})
+       when (y2 == y1 and x2 == x1 - 1) or (y2 == y1 + 1 and x2 == x1),
+       do: true
+
+  defp connecting?({y1, x1, "7"}, {y2, x2, "S"})
+       when (y2 == y1 and x2 == x1 - 1) or (y2 == y1 + 1 and x2 == x1),
+       do: true
+
+  defp connecting?({y1, x1, "F"}, {y2, x2, "|"}) when y2 == y1 + 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "F"}, {y2, x2, "L"}) when y2 == y1 + 1 and x2 == x1, do: true
+  defp connecting?({y1, x1, "F"}, {y2, x2, "-"}) when y2 == y1 and x2 == x1 + 1, do: true
+  defp connecting?({y1, x1, "F"}, {y2, x2, "7"}) when y2 == y1 and x2 == x1 + 1, do: true
+
+  defp connecting?({y1, x1, "F"}, {y2, x2, "J"})
+       when (y2 == y1 + 1 and x2 == x1) or (y2 == y1 and x2 == x1 + 1),
+       do: true
+
+  defp connecting?({y1, x1, "F"}, {y2, x2, "S"})
+       when (y2 == y1 and x2 == x1 + 1) or (y2 == y1 + 1 and x2 == x1),
+       do: true
+
+  defp connecting?({_, _, "S"} = start, other), do: connecting?(other, start)
+  defp connecting?(_, _), do: false
+
+  def find_next_pipe(options, current) do
+    # IO.inspect({current, options}, label: "finding next pipe from ...")
+
+    case Enum.filter(options, fn node -> connecting?(node, current) end) do
+      [] ->
+        nil
+
+      [pipe] ->
+        pipe
+
+      _wtf ->
+        raise "Where the heck did you get that banana ?"
+    end
+  end
+
+  defp find_neighbors(matrix, current) do
+    current
+    |> direct_neighbors_of()
+    |> Enum.map(fn {y, x} -> element_at(matrix, y, x) end)
+  end
+
+  defp parse_matrix(file) do
+    file
+    |> split_lines()
+    |> Enum.map(&String.split(&1, "", trim: true))
+    |> Enum.with_index(&with_tuple/2)
+    |> Enum.map(fn {y_index, row} ->
+      Enum.with_index(row, fn ele, x_index -> {y_index, x_index, ele} end)
+    end)
+  end
+
+  defp with_tuple(element, index), do: {index, element}
+
+  defp element_at(matrix, y, x) do
+    Enum.find(matrix, fn {y_idx, x_idx, _ele} -> y_idx == y && x_idx == x end)
+  end
+
+  defp direct_neighbors_of({y, x, _symbol}) do
+    [
+      # top row
+      {y - 1, x},
+      # middle row
+      {y, x - 1},
+      {y, x + 1},
+      # bottom row
+      {y + 1, x}
+    ]
+  end
+end
--- 2023/elixir/lib/mix/tasks/aoc.ex
+++ 2023/elixir/lib/mix/tasks/aoc.ex
@@ -46,6 +46,10 @@ defmodule Mix.Tasks.Aoc do
     IO.puts(Day09.part_two())
   end
 
+  def run(["10"]) do
+    IO.puts(Day10.part_one())
+  end
+
   @shortdoc "Calculate solution(s) for a given day"
   @moduledoc """
   A custom mix task that runs the code to calculate solutions for Advent of Code 2022
