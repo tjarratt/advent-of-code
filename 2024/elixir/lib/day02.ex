4cefb20b47181007ced0c3dcaa0df5321c9fb319
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-02 09:28:49 +0100

    Little bit of refactoring to make day 2 part 2 a touch easier

--- 2024/elixir/lib/day02.ex
+++ 2024/elixir/lib/day02.ex
@@ -6,21 +6,32 @@ defmodule Day02 do
     |> read_file!()
     |> split_lines()
     |> Enum.map(fn line -> line |> String.split(" ") |> Enum.map(&String.to_integer/1) end)
-    |> Enum.reject(&unsafe?/1)
+    |> Enum.group_by(&safe?/1)
+    |> Map.get(:safe)
     |> Enum.count()
   end
 
-  def unsafe?([head, tail]) do
-    abs(tail - head) not in [1, 2, 3]
-  end
-
-  def unsafe?([head, neck | tail] = reports) do
-    cond do
-      reports != Enum.sort(reports, :asc) and reports != Enum.sort(reports, :desc) -> true
-      unsafe?([head, neck]) -> true
-      true -> unsafe?([neck | tail])
+  def safe?([head, tail]) do
+    if abs(tail - head) in [1, 2, 3] do
+      :safe
+    else
+      :unsafe
     end
   end
 
-  def unsafe?(_), do: false
+  def safe?([head, neck | tail] = reports) do
+    cond do
+      reports != Enum.sort(reports, :asc) and reports != Enum.sort(reports, :desc) ->
+        :unsafe
+
+      safe?([head, neck]) == :unsafe ->
+        :unsafe
+
+      # otherwise
+      true ->
+        safe?([neck | tail])
+    end
+  end
+
+  def safe?(_), do: :safe
 end
