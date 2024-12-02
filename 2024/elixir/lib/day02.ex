6a5a31c430b96b943255fbb7ed44da2421ed704a
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-02 17:33:22 +0100

    2024; elixir; day 2; part 2
    Not quite as easy a layup as I might have hoped, but this
    still isn't so bad. A good refactoring opportunity, for sure.

--- 2024/elixir/lib/day02.ex
+++ 2024/elixir/lib/day02.ex
@@ -11,6 +11,18 @@ defmodule Day02 do
     |> Enum.count()
   end
 
+  def part_two() do
+    %{safe: safe, unsafe: unsafe} =
+      "input"
+      |> read_file!()
+      |> split_lines()
+      |> Enum.map(fn line -> line |> String.split(" ") |> Enum.map(&String.to_integer/1) end)
+      |> Enum.group_by(&safe?/1)
+
+    Enum.count(safe) + count_safe_with_dampening(unsafe)
+  end
+
+  # # # part 1
   def safe?([head, tail]) do
     if abs(tail - head) in [1, 2, 3] do
       :safe
@@ -34,4 +46,20 @@ defmodule Day02 do
   end
 
   def safe?(_), do: :safe
+
+  # # # part 2
+
+  defp count_safe_with_dampening(unsafe_reports) do
+    unsafe_reports
+    |> Enum.filter(fn report ->
+      0..length(report)
+      |> Enum.any?(fn index ->
+        report
+        |> List.delete_at(index)
+        |> safe?()
+        |> then(fn result -> result == :safe end)
+      end)
+    end)
+    |> Enum.count()
+  end
 end
