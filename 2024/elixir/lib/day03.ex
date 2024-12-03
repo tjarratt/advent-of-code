cf0d0108121afb21ddd4796552e3205e11b71d23
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-03 09:08:08 +0100

    2024; elixir; day 3; part 2
    Badda bing, badda boom

--- 2024/elixir/lib/day03.ex
+++ 2024/elixir/lib/day03.ex
@@ -10,6 +10,17 @@ defmodule Day03 do
     |> Enum.sum()
   end
 
+  def part_two() do
+    "input"
+    |> read_file!()
+    |> then(fn input ->
+      Regex.scan(~r[(mul\(\d+,\d+\)|do\(\)|don't\(\))], input, capture: :first)
+    end)
+    |> List.flatten()
+    |> Enum.reduce(%{mul_enabled: true, sum: 0}, &multiply_do_dont/2)
+    |> Map.fetch!(:sum)
+  end
+
   defp multiply(string) do
     [a, b] =
       Regex.scan(~r[mul\((\d+),(\d+)\)], string, capture: :all_but_first)
@@ -17,5 +28,24 @@ defmodule Day03 do
       |> Enum.map(&String.to_integer/1)
 
     a * b
+  end
+
+  def multiply_do_dont(string, state) do
+    cond do
+      state.mul_enabled and string =~ "mul" ->
+        %{state | sum: state.sum + multiply(string)}
+
+      state.mul_enabled == false and string =~ "mul" ->
+        state
+
+      string == "don't()" ->
+        %{state | mul_enabled: false}
+
+      string == "do()" ->
+        %{state | mul_enabled: true}
+
+      true ->
+        raise "unknown operation #{string}"
+    end
   end
 end
