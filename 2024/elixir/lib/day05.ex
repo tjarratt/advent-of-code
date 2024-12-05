03e7e3c14058d51143ee4cfb00cee50af0efe192
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-05 11:32:18 +0100

    2024; elixir; day 5; part 1
    A bit challenging, but once I realized that I could
    look through the pages in the updates in reverse order, this became a fairly
    simple recursive function to check if the updates are out of order.

---
+++ 2024/elixir/lib/day05.ex
@@ -0,0 +1,57 @@
+defmodule Day05 do
+  use AocTemplate
+
+  def part_one() do
+    "input"
+    |> read_file!()
+    |> String.split("\n\n")
+    |> then(fn [rules, updates] -> {parse(rules, :rules), parse(updates, :updates)} end)
+    |> filter_valid()
+    |> Enum.map(&middle_page/1)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+
+  defp filter_valid({rules, updates}) do
+    Enum.reject(updates, fn update ->
+      update |> Enum.reverse() |> out_of_order?(rules)
+    end)
+  end
+
+  # for each page (in reverse order)
+  # check if any of the preceeding pages should have followed it
+  defp out_of_order?([page | earlier_pages], rules) do
+    must_appear_after = Map.get(rules, page, []) |> MapSet.new()
+
+    if MapSet.disjoint?(must_appear_after, MapSet.new(earlier_pages)) do
+      out_of_order?(earlier_pages, rules)
+    else
+      true
+    end
+  end
+
+  # base case, if we've hit here, then no pages appeared before this one
+  defp out_of_order?(_page, _rules), do: false
+
+  # returns the map which yields the pages that must appear after a given page
+  defp parse(rules, :rules) do
+    rules
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> line |> String.split("|") end)
+    |> Enum.reduce(%{}, fn [page, followed_by], acc ->
+      Map.update(acc, page, [followed_by], fn existing -> existing ++ [followed_by] end)
+    end)
+  end
+
+  defp parse(updates, :updates) do
+    updates
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> line |> String.split(",") end)
+  end
+
+  defp middle_page(update) do
+    index = ((length(update) - 1) / 2) |> floor()
+
+    Enum.at(update, index)
+  end
+end
