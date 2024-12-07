9e74e2a800bf3311957460eb250c48ef292435c5
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-07 11:59:30 +0100

    2024; elixir; day 7; part 2
    Slow, but it works!
    
    Takes approximately 15 seconds to compute on my m2 pro, but that's good enough for me.

--- 2024/elixir/lib/day07.ex
+++ 2024/elixir/lib/day07.ex
@@ -11,33 +11,70 @@ defmodule Day07 do
     |> Enum.sum()
   end
 
+  def part_two() do
+    "input"
+    |> read_file!()
+    |> split_lines()
+    |> Enum.map(&parse_equation/1)
+    |> Enum.filter(&solveable?(&1, :part_two))
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
   defp solveable?({test_value, numbers}) do
     numbers
     |> solutions()
     |> Enum.any?(fn equation -> compute(equation) == test_value end)
   end
 
-  def compute([x, :+, y | rest]) do
+  defp solveable?({test_value, numbers}, :part_two) do
+    numbers
+    |> solutions_part_two()
+    |> Enum.any?(fn equation -> compute(equation) == test_value end)
+  end
+
+  defp compute([x, :+, y | rest]) do
     compute([x + y | rest])
   end
 
-  def compute([x, :*, y | rest]) do
+  defp compute([x, :*, y | rest]) do
     compute([x * y | rest])
   end
 
-  def compute([value]), do: value
+  defp compute([x, :||, y | rest]) do
+    result = "#{x}#{y}" |> String.to_integer()
 
-  def solutions([x, y]),
+    compute([result | rest])
+  end
+
+  defp compute([value]), do: value
+
+  defp solutions([x, y]),
     do: [
       [x, :+, y],
       [x, :*, y]
     ]
 
-  def solutions([x | rest]) do
+  defp solutions([x | rest]) do
     solutions = solutions(rest)
 
     Enum.map(solutions, fn soln -> [x, :+ | soln] end) ++
       Enum.map(solutions, fn soln -> [x, :* | soln] end)
+  end
+
+  defp solutions_part_two([x, y]),
+    do: [
+      [x, :+, y],
+      [x, :*, y],
+      [x, :||, y]
+    ]
+
+  defp solutions_part_two([x | rest]) do
+    solutions = solutions_part_two(rest)
+
+    Enum.map(solutions, fn soln -> [x, :+ | soln] end) ++
+      Enum.map(solutions, fn soln -> [x, :* | soln] end) ++
+      Enum.map(solutions, fn soln -> [x, :|| | soln] end)
   end
 
   defp parse_equation(line) do
