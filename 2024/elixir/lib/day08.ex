5ba0102958419ec6771b26d6dc2a44a26be9246a
Tim Jarratt <tim.jarratt@maersk.com>
2024-12-08 17:20:04 +0100

    2024; elixir; day 8 ; part 2
    Lost some time because I didn't realize that all of the
    antennae are automatically resonant antipods, and that I should multiply the dx/dy to each antennae
    starting from a multiple of zero, but aside from that, it wasn't so bad.
    
    Rather fun, even.

--- 2024/elixir/lib/day08.ex
+++ 2024/elixir/lib/day08.ex
@@ -12,6 +12,15 @@ defmodule Day08 do
     |> MapSet.size()
   end
 
+  def part_two() do
+    "input"
+    |> read_file!()
+    |> Grid.parse()
+    |> find_antinodes(:part_two)
+    |> Enum.into(MapSet.new())
+    |> MapSet.size()
+  end
+
   defp antinodes_for(a, b) do
     dx = a.x - b.x
     dy = b.y - a.y
@@ -22,21 +31,47 @@ defmodule Day08 do
     ]
   end
 
+  defp antinodes_for(a, b, :part_two) do
+    dx = a.x - b.x
+    dy = b.y - a.y
+
+    [
+      Stream.unfold(0, fn iteration ->
+        result = %{x: a.x + iteration * dx, y: a.y - iteration * dy}
+
+        {result, iteration + 1}
+      end),
+      Stream.unfold(0, fn iteration ->
+        result = %{x: b.x - iteration * dx, y: b.y + iteration * dy}
+
+        {result, iteration + 1}
+      end)
+    ]
+  end
+
   defp find_antinodes({grid, max_y, max_x}) do
     grid
     |> extract_signals()
     |> Enum.map(&calculate_antinodes(&1, grid))
     |> List.flatten()
-    |> Enum.reject(&exclude_outside(&1, max_y, max_x))
+    |> Enum.filter(&inside_grid?(&1, max_y, max_x))
   end
 
-  defp exclude_outside(antinode, max_y, max_x) do
+  defp find_antinodes({grid, max_y, max_x}, :part_two) do
+    grid
+    |> extract_signals()
+    |> Enum.map(&calculate_antinodes_with_resonant_harmonics(&1, grid))
+    |> List.flatten()
+    |> Enum.flat_map(fn stream -> Stream.take_while(stream, &inside_grid?(&1, max_y, max_x)) end)
+  end
+
+  defp inside_grid?(antinode, max_y, max_x) do
     cond do
-      antinode.x < 0 -> true
-      antinode.x > max_x -> true
-      antinode.y < 0 -> true
-      antinode.y > max_y -> true
-      true -> false
+      antinode.x < 0 -> false
+      antinode.x > max_x -> false
+      antinode.y < 0 -> false
+      antinode.y > max_y -> false
+      true -> true
     end
   end
 
@@ -44,6 +79,12 @@ defmodule Day08 do
     antennaes = grid |> List.flatten() |> Enum.filter(&(&1.character == signal))
 
     for a <- antennaes, b <- antennaes, a != b, do: antinodes_for(a, b)
+  end
+
+  defp calculate_antinodes_with_resonant_harmonics(signal, grid) do
+    antennaes = grid |> List.flatten() |> Enum.filter(&(&1.character == signal))
+
+    for a <- antennaes, b <- antennaes, a != b, do: antinodes_for(a, b, :part_two)
   end
 
   defp extract_signals(grid) do
